a63db983ebe6f7196a705b3af6bf468f565106aa
BirkBerger <theabberger@gmail.com>
2020-09-28 12:38:23 +0200

    added CityRecord class

---
+++ Exercises/Java4/CSVProcessor/src/edu/sdse/csvprocessor/CityRecord.java
@@ -0,0 +1,15 @@
+package edu.sdse.csvprocessor;
+
+public class CityRecord {
+	
+	int id;
+	int year;
+	String city;
+	int population;
+	
+
+	public String toString() {
+		return "id: " + this.id + ", year: " + this.year + ", city: " + this.city + ", population: " + this.population;
+	}
+
+}
